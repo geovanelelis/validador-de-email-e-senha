0c55c87949f9aaeb8ed59aad982be60c519ea4d5
Geovane <geovanelelisds@gmail.com>
2025-01-20 22:32:20 -0300

    validação de email, senha e nome finalizadas

--- index.js
+++ index.js
@@ -6,11 +6,43 @@ function validateEmail(email) {
   }
 }
 
+function validatePassword(password) {
+  if (
+    password.lenght < 8 ||
+    !password.match(/[a-z]/) ||
+    !password.match(/[A-Z]/) ||
+    !password.match(/\d/) ||
+    !password.match(/[^a-zA-Z0-9\s]/)
+  ) {
+    const err = new Error('Senha inválida!')
+    err.input = 'password'
+    throw err
+  }
+}
+
+function validateName(name) {
+  if (name.match(/[^a-zA-Z\s]/)) {
+    const err = new Error('Nome inválido!')
+    err.input = 'name'
+    throw err
+  }
+}
+
 function resetFormStyles(inputs) {
   Object.entries(inputs).forEach(([key, value]) => {
     value.classList.remove('success', 'error')
     document.querySelector(`#${key}-error`).textContent = ''
   })
+}
+
+function registrationSuccess() {
+  setTimeout(() => {
+    alert(`Usuário ${userInputs.name.value} cadastrado com sucesso!`)
+    userInputs.name.value = ''
+    userInputs.email.value = ''
+    userInputs.password.value = ''
+    resetFormStyles(userInputs)
+  }, 1 * 1000)
 }
 
 const userInputs = {
@@ -24,12 +56,36 @@ const form = document.querySelector('form')
 form.addEventListener('submit', (ev) => {
   ev.preventDefault()
   resetFormStyles(userInputs)
+
+  let hasError = false
   try {
-    userInputs.name.classList.add('success')
     validateEmail(userInputs.email.value)
     userInputs.email.classList.add('success')
   } catch (err) {
+    hasError = true
     userInputs[err.input].classList.add('error')
     document.querySelector(`#${err.input}-error`).textContent = err.message
+  }
+
+  try {
+    validatePassword(userInputs.password.value)
+    userInputs.password.classList.add('success')
+  } catch (err) {
+    hasError = true
+    userInputs[err.input].classList.add('error')
+    document.querySelector(`#${err.input}-error`).textContent = err.message
+  }
+
+  try {
+    validateName(userInputs.name.value)
+    userInputs.name.classList.add('success')
+  } catch (err) {
+    hasError = true
+    userInputs[err.input].classList.add('error')
+    document.querySelector(`#${err.input}-error`).textContent = err.message
+  }
+
+  if (!hasError) {
+    registrationSuccess()
   }
 })
